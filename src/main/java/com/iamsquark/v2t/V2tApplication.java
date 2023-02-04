6b72a69b3d44fe7cb5b0caefec3889a7c9a51ee0
aravind9570 <47534432+aravind9570@users.noreply.github.com>
2023-02-04 15:56:50 +0530

    inital commit

---
+++ src/main/java/com/iamsquark/v2t/V2tApplication.java
@@ -0,0 +1,13 @@
+package com.iamsquark.v2t;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class V2tApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(V2tApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iamsquark/v2t/V2tApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iamsquark.v2t;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class V2tApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
